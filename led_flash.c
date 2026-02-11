4b1663316b30e0b59cc5310213b187316c66c386
formation <mailys.pelissier@gmail.com>
2026-02-11 11:29:25 +0100

    ça marche

--- led_flash.c
+++ led_flash.c
@@ -8,79 +8,56 @@
 
 
 
-int up() {
+void up() {
 	PORTB |= (1 << LEDPIN);
-	_delay_us(DURATION);
-	return(0);
 }
 
 
-int down() {
+void down() {
 	PORTB &= ~ (1 << LEDPIN);
-	_delay_us(DURATION);
-	return(0);
 }
 
 // 	char c = '65';
 
-void convert( char c) {
-	for (int i = 0; i<8; i++) {
-		
-		if(c & 1) {
-			up();
-			}
-		else {
-			down();
-		
-		}
-		
-		c = c >> 1;
-	}
-}
+void convert(char c) {
+    down();
+    _delay_us(DURATION);
+    for (int i = 0; i < 8; i++) {
+        if(c & 1) {
+            up();
+        }
+        else {
+            down();
+        }
+        c = c >> 1;
+    _delay_us(DURATION);
+    }
+    
+    up(); 
+    _delay_us(DURATION);  }
 
 
 void convert_str(char * str) {
-	
-	int end = strlen(str);
-	
-	for (int i =0; i < end ; i++) {
-		convert(str[i]);
-	}
+    int end = strlen(str);
+    
+    for (int i = 0; i < end; i++) {
+        convert(str[i]);
+    }
 }
 
+int main() {
+    DDRB |= (1 << LEDPIN);  //PB4 en sortie
+    PORTB |= (1 << LEDPIN); // HAUT
+    
+    _delay_ms(DURATION);  // premier delai
+    
+    while(1) {
+        convert_str("test");
+        _delay_ms(1000);  
+    }
+    
+    return 0;
+}
 
-int main () {
-
-	while(1) {
-
-		DDRB |= (1 << LEDPIN); // mettre PB4 en sortie 
-		
-		PORTB |= (1 << LEDPIN); // LED allumée par défaut
-
-		
-		// état initial
-		for (int i=0 ;i<1000 ; i++) {
-		
-			 up();
-		}
-		
-		// start 
-		
-		down();
-		
-		convert(65);
-		
-		//stop 
-		
-		for (int i=0 ;i<1000 ; i++) {
-		
-			 up();
-		}
-		
-	}
-	
-		
-	return(0);
-};
 
 
